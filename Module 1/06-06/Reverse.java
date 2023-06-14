23b4e960cb895e50df8db9b1aa8ce558212d4623
gohil rahul <118652650+gohilrahulv@users.noreply.github.com>
2023-06-14 09:26:47 +0530

    Add files via upload

---
+++ Module 1/06-06/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Reverse 
+{
+    public static void main(String[]args)
+    {   int reverse1=0;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter The Number : ");
+        int n =sc.nextInt();
+        while(n !=0)
+        {
+            int r = n%10;
+            reverse1 = reverse1*10+r;
+            n=n/10;
+            
+        }
+        System.out.println("The reverse of the given number is: " + reverse1);   
+    }
+}
---
+++ Module 1/06-06/Whileloop.java
@@ -0,0 +1,12 @@
+public class Whileloop
+{
+    public static void main(String[]args)
+    {
+        int i=51;
+        while(i<=60)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
