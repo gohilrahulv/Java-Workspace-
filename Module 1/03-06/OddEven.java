e280136880954a5b5d51c1045466798c2599029d
gohil rahul <118652650+gohilrahulv@users.noreply.github.com>
2023-06-14 09:09:35 +0530

    Add files via upload

---
+++ Module 1/03-06/OddEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddEven 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Num : ");
+        int i = sc.nextInt();
+
+        if (i%2==0)
+        {
+            System.out.println("The Number Is Even.");
+        }
+        else
+        {
+            System.out.println("The Number Is Odd.");
+        }
+    }    
+}
---
+++ Module 1/03-06/SquareCube.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SquareCube 
+{
+    public static void main(String args[])
+    {
+        int n,a,b,c;
+        System.out.println("Enter the num : ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println("Square of "+n+" Is : "+ Math.pow(n, 2));
+        System.out.println("Cube of "+n+" Is : "+ Math.pow(n, 3));
+        //-------------Second Method----------------
+        System.out.println("---------------------------------------------");
+        a=n;
+        b=n*n;
+        c=n*n*n;
+        System.out.println("Square of "+n+" Is : "+ b);
+        System.out.println("Cube of "+n+" Is : "+ c);
+    }    
+}
