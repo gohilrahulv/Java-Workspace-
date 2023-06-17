d146b13e71c3281137793ad2ca18af8fd1b10589
gohil rahul <118652650+gohilrahulv@users.noreply.github.com>
2023-06-17 10:29:10 +0530

    Add files via upload

---
+++ Module 1/06-06/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String args[])
+    {
+        int n1=0,n2=1,n3,i,num;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter The Num : ");
+        num =s.nextInt();
+        System.out.print(n1 +" "+ n2);
+        for (i=2;i<num;i++)
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
---
+++ Module 1/06-06/Summation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Summation 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner sc = new Scanner(System.in); 
+
+        int num, remainder, sum=0; 
+
+        System.out.print("Enter Number : ");
+        num = sc.nextInt(); 
+
+        while(num > 0)
+        {
+            remainder = num % 10; 
+            sum = sum - remainder; 
+            num = num / 10; 
+        }
+        System.out.println("Summation of Digits : " + sum);
+    }
+}
