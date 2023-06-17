71d982992c303ad01f6e8bd1be2aa7636dda867a
gohil rahul <118652650+gohilrahulv@users.noreply.github.com>
2023-06-17 11:03:36 +0530

    Add files via upload

---
+++ Module 1/06-06/FindMax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FindMax 
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter The Number : ");
+        int num = s.nextInt();
+
+        int max =num%10;
+        while(num>0)
+        {
+            if(num%10>max)
+            {
+                max=num%10;
+            }
+            num = num/10;
+        }
+        System.out.println("Max Digit Is : "+max);
+    }
+}
